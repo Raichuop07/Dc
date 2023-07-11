d516d3fa6903b0f77ca52da71b479bfad557d210
Satyam <138608348+Raichu07@users.noreply.github.com>
2023-07-12 01:29:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "1356469075").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Raichuop07/Hero")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Raichu07/Dec")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
